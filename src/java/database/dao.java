480d0db42c55887861bd8f13803e75589129c341
quanndhe181858 <quanndhe181858@fpt.edu.vn>
2025-12-04 13:30:00 +0700

    Create dao.java

---
+++ src/java/database/dao.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author quan
+ */
+public class dao {
+
+    DBContext dbc = new DBContext();
+    Connection con = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    protected boolean closeResources() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+            return true;
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            return false;
+        }
+    }
+
+}
